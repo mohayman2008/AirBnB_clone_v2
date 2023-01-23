e4c3312e62e42ac57abbba3cd7afab3007630551
Mohamed Ayman <mohayman2007@gmail.com>
2023-01-23 09:47:10 +0000

    AirBnB Clone 0x03 - Deploy static - Tasks 0 & 1

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# Bash script that sets up the web servers for the deployment of web_static
+# shellcheck disable=SC1004
+
+# Install Nginx if it not already installed
+nginx -v || (apt-get update && apt-get -y install nginx)
+
+# Create the directories tree
+mkdir -p '/data/web_static/releases/test/'
+mkdir -p '/data/web_static/shared/'
+
+# Deploy the current test realease
+ln -sf '/data/web_static/releases/test/' '/data/web_static/current'
+
+# Manage the ownership of '/data' directory and all its contents
+chown -R 'ubuntu':'ubuntu' '/data'
+
+# Create a test index.html
+echo 'AirBnB clone' > /data/web_static/releases/test/index.html
+
+# Configure nginx to serve the contents of '/data/web_static/current/'
+# + to location '/hbnb_static/'
+CONF_FILE='/etc/nginx/sites-available/default'
+rule_blk=\
+'	location /hbnb_static/ {\
+		alias /data/web_static/current/;\
+	}'
+rule='	alias /data/web_static/current/;\n'
+if [ "$(grep -c -E '^\s*location\s*/hbnb_static/\s*{[ \t]*$' "$CONF_FILE")" -eq 0 ]; then
+	sed -z -E -i 's@(\n?([ \t]*)location\s*/\s*\{[^}]*\})@\1\n\n'"$rule_blk@" "$CONF_FILE"
+else
+	sed -z -E -i 's@(\n?([ \t])*location\s*/hbnb_static/\s*\{)[^}]*\}@\1\n\2'"$rule"'\2\}@' "$CONF_FILE"
+fi
+
+# Make sure that default configuration is enabled 
+ln -sf "$CONF_FILE" /etc/nginx/sites-enabled/default
+
+# Restart Nginx service 
+service nginx restart
---
+++ 1-pack_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz archive from the contents of
+the 'web_static' folder"""
+from datetime import datetime
+
+from fabric.api import env, local, run, sudo, put
+
+env.user = 'ubuntu'
+web1 = '54.90.15.29'
+web2 = '18.207.207.53'
+env.hosts = [web1, web2]
+
+
+def do_pack():
+    '''Packs the contents of the 'web_static' folder to a .tgz archive'''
+
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    directory = 'versions'
+    filename = 'web_static_' + timestamp + '.tgz'
+    path = f'{directory}/{filename}'
+
+    local(f'mkdir -p {directory}')
+
+    if (local(f'tar -cvzf {directory}/{filename} web_static').failed):
+        return None
+    return path
+
+
+def set_up():
+    '''Sets up the web servers for the deployment of web_static using
+    "0-setup_web_static.sh" bash script'''
+
+    put('0-setup_web_static.sh', mirror_local_mode=True)
+    code = sudo('./0-setup_web_static.sh').return_code
+    run('rm -f 0-setup_web_static.sh')
+
+    print(f'Set up script was run and exited with exit code {code}\n')
