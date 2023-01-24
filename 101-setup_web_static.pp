12cef8ee7c0a9d9b92f51dbf400f8f56d2375a5e
Mohamed Ayman <mohayman2007@gmail.com>
2023-01-24 02:09:06 +0000

    AirBnB Clone 0x03 - Deploy static - Task 5 - "Puppet for setup"

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,92 +1,63 @@
-# Install Nginx if it not already installed
-exec {'apt-get update':
-  path => $path
-}
+# Puppet script that sets up the web servers for the deployment of web_static
 
-package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt-get update'],
-  before  => Exec['add nginx firewall rule']
-}
-
-exec { 'add nginx firewall rule':
+exec {'update':
   provider => shell,
-  command  => 'ufw allow \'Nginx HTTP\'',
-  path     => $path
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
-####
 
-# Manage the ownership of '/data' directory and all its contents
-file { '/data':
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['start Nginx'],
+}
+
+exec {'start Nginx':
+  provider => shell,
+  command  => 'sudo service nginx start',
+  before   => Exec['create first directory'],
+}
+
+exec {'create first directory':
+  provider => shell,
+  command  => 'sudo mkdir -p /data/web_static/releases/test/',
+  before   => Exec['create second directory'],
+}
+
+exec {'create second directory':
+  provider => shell,
+  command  => 'sudo mkdir -p /data/web_static/shared/',
+  before   => Exec['content into html'],
+}
+
+exec {'content into html':
+  provider => shell,
+  command  => 'echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html',
+  before   => Exec['symbolic link'],
+}
+
+exec {'symbolic link':
+  provider => shell,
+  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  before   => Exec['put location'],
+}
+
+exec {'put location':
+  provider => shell,
+  command  => "sudo sed -i \'38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\' \
+  /etc/nginx/sites-available/default",
+  before   => Exec['restart Nginx'],
+}
+
+exec {'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+  before   => File['/data/']
+}
+
+file {'/data/':
   ensure  => directory,
-  recurse => true,
   owner   => 'ubuntu',
-  group   => 'ubuntu'
-}
-
-# Create the directories tree
-file { '/data/web_static':
-  ensure => directory
-}
-
-file { '/data/web_static/releases':
-  ensure => directory
-}
-
-file { '/data/web_static/shared/':
-  ensure => directory
-}
-
-file { '/data/web_static/releases/test':
-  ensure => directory
-}
-####
-
-# Deploy the current test realease
-file { 'create current symlink':
-  ensure => link,
-  path   => '/data/web_static/current',
-  target => '/data/web_static/releases/test'
-}
-
-# Create a test index.html
-file { '/data/web_static/releases/test/index.html':
-  ensure  => present,
-  content => "AirBnB clone\n"
-}
-# exec { 'create index.html':
-#   command => 'echo "AirBnB clone" > /data/web_static/releases/test/index.html',
-#   path    => $path
-# }
-
-# Configure nginx to serve the contents of '/data/web_static/current/'
-# + to location '/hbnb_static/' '\''
-exec { 'nginx config':
-  path     => $path,
-  provider => shell,
-  notify   => Service['nginx'],
-  command  => 'bash -c \'
-CONF_FILE=/etc/nginx/sites-available/default
-rule_blk="\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
-rule="\talias /data/web_static/current/;\n"
-if [ "$(grep -c -E "^\\\\s*location\\\\s*/hbnb_static/\\\\s*{[ \\\\t]*$" "$CONF_FILE")" -eq 0 ]; then
-	sudo sed -z -E -i "s@(\\\\n?([ \\\\t]*)location\\\\s*/\\\\s*\\\\{[^}]*\\\\})@\\\\1\\\\n\\\\n$rule_blk@" "$CONF_FILE"
-else
-	sudo sed -z -E -i "s@(\\\\n?([ \\\\t])*location\\\\s*/hbnb_static/\\\\s*\\\\{)[^}]*\\\\}@\\\\1\\\\n\\\\2$rule\\\\2\}@" "$CONF_FILE"
-fi
-  \''
-}
-
-# Make sure that default configuration is enabled
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default'
-}
-
-# Restart Nginx service 
-service { 'nginx':
-  ensure  => running,
-  path    => $path,
-  restart => 'service nginx restart',
-  require => Package['nginx']
+  group   => 'ubuntu',
+  recurse => true,
 }
