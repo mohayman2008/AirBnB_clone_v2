64439308e3e41a097a1cb4ff11df5960738f5021
Mohamed Ayman <mohayman2007@gmail.com>
2023-02-07 00:34:13 +0000

    AirBnB Clone 0x04 - Web framework - Tasks 7 & 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,7 +16,11 @@ def states_list():
     return render_template('7-states_list.html', states=states)
 
 
-storage.close = app.teardown_appcontext(storage.close)
+@app.teardown_appcontext
+def storage_close(*args, **kwargs):
+    '''Teardown operations'''
+    storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
