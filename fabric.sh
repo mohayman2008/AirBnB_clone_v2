5a298a51452436524ec80f945c75bd77a71942a0
Mohamed Ayman <mohayman2007@gmail.com>
2024-02-07 08:39:42 +0000

    0x02. AirBnB Clone (MySQL) - Task 6 - Remove f strings

---
+++ fabric.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+apt-get install -y libffi-dev libssl-dev build-essential python3.4-dev libpython3-dev
+pip3 install pyparsing appdirs setuptools==40.1.0 cryptography==2.8 bcrypt==3.1.7 PyNaCl==1.3.0 Fabric3==1.14.post1
